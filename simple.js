f5919efaa4897162a642ef77a27ea6a50af6d7cb
Palash Halder <halder.palash08@gmail.com>
2023-03-27 07:15:10 +0600

    explanation given

--- simple.js
+++ simple.js
@@ -25,9 +25,24 @@ console.log(sum);
 // Doing the same thing with function
 
 function sum(i) {
-    console.log(i);
+    if(i == 1){
+        return 1;
+    }
     return i + sum(i-1);
 }
 const result = sum(5);
 console.log(result);
+
+
+/*
+
+5 + sum(4)
+5 + 4 + sum(3)
+5 + 4 + 3 + sum(2)
+5 + 4 + 3 + 2 + sum(1)
+5 + 4 + 3 + 2 + 1 [will stop here]
+= 15
+
+
+*/
 
